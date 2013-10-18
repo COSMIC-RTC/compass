15a9b486ba34c9fde1dc8055932138c35296821f
Sevin Arnaud <Arnaud.Sevin@obspm.fr>
2013-10-18 15:05:21 +0000

    small update for kepler and cula R17

--- define_var.sh
+++ define_var.sh
@@ -18,3 +18,5 @@ export YOGA_AO_TOP=$YOGA_AO_DIR
 
 export LD_LIBRARY_PATH=$YOGA_DIR/libyoga:$YOGA_AO_DIR/libyoga_ao:$CUDA_LIB_PATH_64:$CUDA_LIB_PATH:$CULA_LIB_PATH_64:$CULA_LIB_PATH #:$LD_LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=/usr/local/cuda/include
+
+alias yorick=~/yorick.git/relocate/bin/yorick
