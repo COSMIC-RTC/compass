7148693169d34150602adf94e53639ec3a34ab86
Gratadour Damien <Damien.Gratadour@obspm.fr>
2013-12-04 14:29:33 +0000

    debugs

--- yoga_template/yoga_template.cpp
+++ yoga_template/yoga_template.cpp
@@ -5,8 +5,8 @@
 #include "magma_lapack.h"
 //#include "testings.h"
 
-//#define magma_dsyevd_gpu_t	magma_dsyevd_gpu		// slow dsymv from cublas legacy api
-#define magma_dsyevd_gpu_t	magma_dsyevd_gpu_magmablas	// fast dsymv from magma
+#define magma_dsyevd_gpu_t	magma_dsyevd_gpu		// slow dsymv from cublas legacy api
+//#define magma_dsyevd_gpu_t	magma_dsyevd_gpu_magmablas	// fast dsymv from magma
 //#define magma_dsyevd_gpu_t	magma_dsyevd_gpu_kblas		// fast dsymv from kblas
 
 extern "C" {
@@ -31,13 +31,14 @@ extern "C" {
   void
   Y_yoga_magma_evd(int argc)
   {
+    int yType;
     long ntot;
     long dims[Y_DIMSIZE];
     if (yarg_subroutine()) {
       // retreive matrix and parameters from yorick session
       char jobz = ygets_c(argc-1);
       char uplo = ygets_c(argc-2);
-      double *h_A = ygeta_any(argc - 3, &ntot, &dims,&yType);
+      void *h_A = ygeta_any(argc - 3, &ntot, dims,&yType);
       if (yType != Y_DOUBLE) y_error("Please provide a double precision array\n");
 
       // d_R becomes a carma object retreived from Yorick 
@@ -52,7 +53,6 @@ extern "C" {
       magma_int_t *iwork;
 
       magma_int_t N, n2, info, lwork, liwork, lda, ldda, aux_iwork[1];
-      magma_int_t izero    = 0;
       magma_int_t ione     = 1;
       magma_int_t ISEED[4] = {0,0,0,1};
       double      aux_work[1];
@@ -62,9 +62,15 @@ extern "C" {
       lda  = N;
       ldda = ((N + 31)/32)*32;
 
-      /* Query for workspace sizes */
+      // eigenvalues are pushed on the interpreter stack
+      //magma_malloc_cpu( (void**) &w1, (N)*sizeof(double) );
+      long dims_eigen[2];
+      dims_eigen[0] = 1; dims_eigen[1] = N; 
+      double *w1 = ypush_d(dims_eigen);
+
+       /* Query for workspace sizes */
       magma_dsyevd_gpu_t( jobz, uplo,
-			  N, d_R->get_data(), ldda, w1,
+			  N, d_R->getData(), ldda, w1,
 			  h_R, lda,
 			  aux_work,  -1,
 			  aux_iwork, -1,
@@ -72,23 +78,17 @@ extern "C" {
       lwork  = (magma_int_t) aux_work[0];
       liwork = aux_iwork[0];
 
-      // eigenvalues are pushed on the interpreter stack
-      //magma_malloc_cpu( (void**) &w1, (N)*sizeof(double) );
-      long *dims_eigen[2];
-      dims_eigen[0] = 1; dims_eigen[1] = N; 
-      double *w1 = ypush_d(dims_eigen);
-
       cudaMallocHost( (void**) &h_R, (N*lda)*sizeof(double) );
       cudaMallocHost( (void**) &h_work, (lwork)*sizeof(double) );
       posix_memalign( (void**) &iwork, 32, (liwork)*sizeof(int) );
       //magma_malloc_cpu( (void**) &iwork, (liwork)*sizeof(magma_int_t) );
 
       /* Initialize the matrix */
-      lapackf77_dlarnv( &ione, ISEED, &n2, h_A );
-      magma_dsetmatrix( N, N, h_A, lda, d_R->get_data(), ldda );
+      lapackf77_dlarnv( &ione, ISEED, &n2, (double *)h_A );
+      magma_dsetmatrix( N, N, (double *)h_A, lda, d_R->getData(), ldda );
 
       magma_dsyevd_gpu_t( jobz, uplo,
-			  N, d_R->get_data(), ldda, w1,
+			  N, d_R->getData(), ldda, w1,
 			  h_R, lda,
 			  h_work, lwork,
 			  iwork, liwork,
