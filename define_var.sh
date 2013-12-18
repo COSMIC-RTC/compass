7d56a224403f1c86ca9e077199cf9fb1bbf9c915
Sevin Arnaud <Arnaud.Sevin@obspm.fr>
2013-12-18 12:33:21 +0000

    echo config to add into .bashrc

--- define_var.sh
+++ define_var.sh
@@ -1,5 +1,6 @@
 
 # CUDA default definitions
+#${STATE?"Need to set STATE"}
 export CUDA_ROOT=/usr/local/cuda
 export CUDA_INC_PATH=$CUDA_ROOT/include
 export CUDA_LIB_PATH=$CUDA_ROOT/lib
@@ -12,7 +13,7 @@ export CULA_LIB_PATH=$CULA_ROOT/lib
 export CULA_LIB_PATH_64=$CULA_ROOT/lib64
 
 # YOGA default definitions
-export COMPASS_ROOT_DIR=$(pwd)#/home/brujo/Yorick/compass/trunk
+export COMPASS_ROOT_DIR=$(pwd)
 export YOGA_DIR=$COMPASS_ROOT_DIR/yoga_ao
 export YOGA_AO_DIR=$YOGA_DIR/yoga_ao
 export YOGA_AO_TOP=$YOGA_AO_DIR
@@ -24,3 +25,29 @@ export YORICK_PATH=~/yorick.git/relocate/bin
 export PATH=$YORICK_PATH:$PATH
 #alias yorick="rlwrap yorick"
 
+echo "COMPASS will use this configuration, please add those lines into the .bashrc"
+
+echo "# CUDA default definitions"
+echo "export CUDA_ROOT=$CUDA_ROOT"
+echo "export CUDA_INC_PATH=\$CUDA_ROOT/include"
+echo "export CUDA_LIB_PATH=\$CUDA_ROOT/lib"
+echo "export CUDA_LIB_PATH_64=\$CUDA_ROOT/lib64"
+echo
+echo "# CULA default definitions"
+echo "export CULA_ROOT=$CULA_ROOT"
+echo "export CULA_INC_PATH=\$CULA_ROOT/include"
+echo "export CULA_LIB_PATH=\$CULA_ROOT/lib"
+echo "export CULA_LIB_PATH_64=\$CULA_ROOT/lib64"
+echo
+echo "# YOGA default definitions"
+echo "export COMPASS_ROOT_DIR=$COMPASS_ROOT_DIR"
+echo "export YOGA_DIR=\$COMPASS_ROOT_DIR/yoga_ao"
+echo "export YOGA_AO_DIR=\$YOGA_DIR/yoga_ao"
+echo "export YOGA_AO_TOP=\$YOGA_AO_DIR"
+echo
+echo "export LD_LIBRARY_PATH=\$COMPASS_ROOT_DIR/libcarma:\$COMPASS_ROOT_DIR/libsutra:\$CUDA_LIB_PATH_64:\$CUDA_LIB_PATH:\$CULA_LIB_PATH_64:\$CULA_LIB_PATH:\$LD_LIBRARY_PATH"
+echo "export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH"
+echo
+echo "export YORICK_PATH=$YORICK_PATH"
+echo "export PATH=\$YORICK_PATH:\$PATH"
+echo 'alias yorick="rlwrap yorick"'
