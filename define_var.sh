4a7bbcb7a4b2dd6a20652748c53325240869eafa
Doucet Nicolas <Nicolas.Doucet@obspm.fr>
2015-11-10 12:03:56 +0000

    update define_var.sh

--- define_var.sh
+++ define_var.sh
@@ -2,8 +2,9 @@
 # CUDA default definitions
 #export GENCODE='"arch=compute_12,code=sm_12" -DGPUSHMEM=120' #for old tesla
 #export GENCODE='"arch=compute_20,code=sm_20" -DGPUSHMEM=200' #for Fermi
-export GENCODE='"arch=compute_30,code=sm_30" -DGPUSHMEM=300' #for Kepler 
+#export GENCODE='"arch=compute_30,code=sm_30" -DGPUSHMEM=300' #for Kepler 
 #export GENCODE='"arch=compute_35,code=sm_35" -DGPUSHMEM=300' #for K40
+export GENCODE='"arch=compute_37,code=sm_37" -DGPUSHMEM=300' #for K40
 #${GENCODE?"Need to set GENCODE"}
 
 export CUDA_ROOT=$CUDA_ROOT #/usr/local/cuda
@@ -23,19 +24,25 @@ export CULA_LIB_PATH_64= #$CULA_ROOT/lib64
 # MAGMA definitions (uncomment this line if MAGMA is installed)
 export MAGMA_ROOT=$MAGMA_ROOT
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MAGMA_ROOT/lib
+export PKG_CONFIG_PATH=$MAGMA_ROOT/lib/pkgconfig
+
 
 # YOGA default definitions
-export COMPASS_ROOT_DIR=$(pwd)
-export YOGA_DIR=$COMPASS_ROOT_DIR/yoga
-export YOGA_AO_DIR=$COMPASS_ROOT_DIR/yoga_ao
+export COMPASS_ROOT=$(pwd)
+export YOGA_DIR=$COMPASS_ROOT/yoga
+export YOGA_AO_DIR=$COMPASS_ROOT/yoga_ao
 export YOGA_AO_TOP=$YOGA_AO_DIR
 
-export LD_LIBRARY_PATH=$COMPASS_ROOT_DIR/libcarma:$COMPASS_ROOT_DIR/libsutra:$CUDA_LIB_PATH_64:$CUDA_LIB_PATH:$CULA_LIB_PATH_64:$CULA_LIB_PATH:$LD_LIBRARY_PATH
+export NAGA_ROOT=$COMPASS_ROOT/naga
+export SHESHA_ROOT=$COMPASS_ROOT/shesha
+
+export LD_LIBRARY_PATH=$COMPASS_ROOT/libcarma:$COMPASS_ROOT/libsutra:$CUDA_LIB_PATH_64:$CUDA_LIB_PATH:$CULA_LIB_PATH_64:$CULA_LIB_PATH:$LD_LIBRARY_PATH
 
 export YORICK_PATH=$YORICK_ROOT
 export PATH=$YORICK_PATH:$PATH
 #alias yorick="rlwrap -s 2000 yorick"
 
+export PYTHONPATH=$SHESHA:$NAGA:$PYTHONPATH
 
 echo "COMPASS will use this configuration, please add those lines into the .bashrc"
 
