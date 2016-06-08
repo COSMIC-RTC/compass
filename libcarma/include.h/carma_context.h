9be2976f38c076d63cc0818e27263601f0c6913a
Sevin Arnaud <Arnaud.Sevin@obspm.fr>
2016-06-08 12:21:21 +0000

    update carma_context

--- libcarma/include.h/carma_context.h
+++ libcarma/include.h/carma_context.h
@@ -165,39 +165,40 @@ public:
 
 /// from /usr/local/cuda/samples/common/inc/helper_cuda.h
 inline int ConvertSMVer2Cores(int major, int minor) {
-  // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
-  typedef struct {
-    int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
-    int Cores;
-  } sSMtoCores;
+    // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+    typedef struct
+    {
+        int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+        int Cores;
+    } sSMtoCores;
 
-  sSMtoCores nGpuArchCoresPerSM[] = { { 0x10, 8 }, // Tesla Generation (SM 1.0) G80 class
-      { 0x11, 8 }, // Tesla Generation (SM 1.1) G8x class
-      { 0x12, 8 }, // Tesla Generation (SM 1.2) G9x class
-      { 0x13, 8 }, // Tesla Generation (SM 1.3) GT200 class
-      { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
-      { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
-      { 0x30, 192 }, // Kepler Generation (SM 3.0) GK10x class
-      { 0x35, 192 }, // Kepler Generation (SM 3.5) GK11x class
-      { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
-      { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
-      { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
-      { -1, -1 } };
+    sSMtoCores nGpuArchCoresPerSM[] =
+    {
+        { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+        { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+        { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
+        { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class
+        { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
+        { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
+        { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
+        { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
+        {   -1, -1 }
+    };
 
-  int index = 0;
+    int index = 0;
 
-  while (nGpuArchCoresPerSM[index].SM != -1) {
-    if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor)) {
-      return nGpuArchCoresPerSM[index].Cores;
+    while (nGpuArchCoresPerSM[index].SM != -1)
+    {
+        if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor))
+        {
+            return nGpuArchCoresPerSM[index].Cores;
+        }
+
+        index++;
     }
 
-    index++;
-  }
-
-  // If we don't find the values, we default use the previous one to run properly
-  printf(
-      "MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n",
-      major, minor, nGpuArchCoresPerSM[index-1].Cores);
-  return nGpuArchCoresPerSM[index-1].Cores;
+    // If we don't find the values, we default use the previous one to run properly
+    printf("MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n", major, minor, nGpuArchCoresPerSM[index-1].Cores);
+    return nGpuArchCoresPerSM[index-1].Cores;
 }
 #endif // _CARMA_CONTEXT_H_
