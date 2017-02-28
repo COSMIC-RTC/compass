4ba48570a92423be8bfc912da35e8fc1b883a6f3
Durand Sebastien <Sebastien.Durand@obspm.fr>
2017-02-28 15:14:47 +0000

    correct dm

--- shesha/src/shesha_dms.pyx
+++ shesha/src/shesha_dms.pyx
@@ -491,7 +491,7 @@ cpdef read_influ_hdf5 (Param_dm p_dm,Param_tel p_tel, Param_geom geom):
     res_h5_m = (res_h5[0]+res_h5[1])/2.
 
     # a introduire dm diameter
-    diam_dm_h5 = h5_tp[p_dm.diam_dm][0]*2
+    diam_dm_h5 = h5_tp[p_dm.diam_dm][0]
     #diam_dm_h5 = [2.54,2.54] # metre
     diam_dm_pup_h5 = h5_tp[p_dm.diam_dm_proj][0]
     #diam_dm_pup_h5 = [43.73,43.73] #metre
