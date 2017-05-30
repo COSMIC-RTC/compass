b7cf30e3bb3fb3708ba912c2cf46ddbadf829330
fvidal <fabrice.vidal@obspm.fr>
2017-05-30 14:56:04 +0200

    Updated 39m PYR par file with Error budget

--- shesha/data/par/MICADO/micado_39m_PYR_ELTPupil.py
+++ shesha/data/par/MICADO/micado_39m_PYR_ELTPupil.py
@@ -15,16 +15,16 @@ p_geom.set_zenithangle(0.)
 
 # tel
 p_tel = ao.Param_tel()
-p_tel.set_diam(39.)
+p_tel.set_diam(38.542)
 p_tel.set_cobs(0.28)
 
 #E_ELT PUPIL
 
 
 p_tel.set_type_ap("EELT-Nominal")
-#p_tel.set_spiders_type("six")
+p_tel.set_spiders_type("six")
 p_tel.set_pupangle(0)
-p_tel.set_t_spiders(0.)
+p_tel.set_t_spiders(0)
 """
 p_tel.set_nbrmissing(7)
 p_tel.set_referr(0.01)
@@ -53,23 +53,23 @@ p_atmos.set_L0([25.])  # Not simulated in Yorick?
 #p_target.set_mag([4.])
 
 # 3 Lambda targets
-p_target=ao.Param_target()
-p_target.set_nTargets(3)
-p_target.set_xpos([0, 0, 0])
-p_target.set_ypos([0, 0, 0])
-p_target.set_Lambda([1.2, 1.65, 2.2])
-p_target.set_mag([4, 4., 4])
+#p_target=ao.Param_target()
+#p_target.set_nTargets(3)
+#p_target.set_xpos([0, 0, 0])
+#p_target.set_ypos([0, 0, 0])
+#p_target.set_Lambda([1.2, 1.65, 2.2])
+#p_target.set_mag([4, 4., 4])
 
 # 2 Lambda targets
-#p_target=ao.Param_target()
-#p_target.set_nTargets(2)
-#p_target.set_xpos([0, 0])
-#p_target.set_ypos([0, 0])
-#p_target.set_Lambda([1.2, 1.65, 2.2])
-#p_target.set_mag([4, 4.])
+p_target=ao.Param_target()
+p_target.set_nTargets(2)
+p_target.set_xpos([0, 0])
+p_target.set_ypos([0, 0])
+p_target.set_Lambda([0.7, 2.2])
+p_target.set_mag([4, 4.])
 # wfs
-#p_wfs0 = ao.Param_wfs(error_budget=True)
-p_wfs0= ao.Param_wfs()
+p_wfs0 = ao.Param_wfs(error_budget=True)
+#p_wfs0= ao.Param_wfs()
 p_wfss = [p_wfs0]
 
 
@@ -80,10 +80,10 @@ p_wfs0.set_fracsub(0.8)
 p_wfs0.set_xpos(0.)
 p_wfs0.set_ypos(0.)
 p_wfs0.set_Lambda(0.7)
-p_wfs0.set_gsmag(11)
+p_wfs0.set_gsmag(14.7)
 p_wfs0.set_optthroughput(0.5)
 p_wfs0.set_zerop(2.6e10) # 2.6e10 ph/s/m**2 computed by Rico in R band for MOSAIC
-p_wfs0.set_noise(-1)  # in electrons units
+p_wfs0.set_noise(0.1)  # in electrons units
 p_wfs0.set_atmos_seen(1)
 p_wfs0.set_fstop("square")
 p_wfs0.set_fssize(1.6)
@@ -93,7 +93,14 @@ p_wfs0.set_pyr_npts(int(np.ceil(int(rMod*2* 3.141592653589793)/4.)*4))
 p_wfs0.set_pyr_ampl(rMod)
 #p_wfs0.set_pyr_pup_sep(int(2 / 3. * p_wfs0.nxsub)) # diffraction effect
 #p_wfs0.set_pyr_pup_sep((p_wfs0.nxsub))
-p_wfs0.set_pyr_pup_sep((64))
+
+"""
+With 52 pixels of margin between 2 edges of pupils on a 240x240 detector and 92 pixels of pupil:
+in Compass pupsep is separation between 1 pupil center and Half of detector
+pupsep = 52/2+92/2 = 72
+"""
+p_wfs0.set_pyr_pup_sep(72)
+
 
 # dm
 p_dm0 = ao.Param_dm()
@@ -113,6 +120,8 @@ p_dm0.set_unitpervolt(1)
 p_dm0.set_push4imat(0.01)
 p_dm0.set_pattern("hexa")
 #p_dm0.set_influType("gaussian")
+p_dm0.set_influType("radialSchwartz")
+
 """
 p_dm0.set_file_influ_hdf5("/home/fvidal/compass/shesha/data/M4data/elt_influ_spider.h5")
 p_dm0.set_center_name("center")
@@ -152,6 +161,7 @@ p_controllers = [p_controller0]
 
 #p_controller0.set_type("ls")
 p_controller0.set_type("generic")
+#p_controller0.set_type("geo")
 
 p_controller0.set_nwfs([0])
 p_controller0.set_ndm([0, 1])
