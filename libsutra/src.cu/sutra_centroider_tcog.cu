33c7ff3736275d55e4caf7eed3f9a77b4242ef54
Sevin Arnaud <Arnaud.Sevin@obspm.fr>
2019-01-30 16:09:11 +0100

    debug sutra_centroider

--- libsutra/src.cu/sutra_centroider_tcog.cu
+++ libsutra/src.cu/sutra_centroider_tcog.cu
@@ -24,7 +24,6 @@ __global__ void centroids(T *d_img, T *d_centroids, T *ref, int *validx,
   unsigned int yvalid = validy[blockIdx.x];
   unsigned int x, y;
   int idim;
-  T data_thresh;
 
   for (int cc = 0; cc < nelem_thread; cc++) {
     x = ((tid * nelem_thread + cc) % npix);
@@ -33,7 +32,7 @@ __global__ void centroids(T *d_img, T *d_centroids, T *ref, int *validx,
     // blockIdx.x;
     idim = (x + xvalid) + (y + yvalid) * size;
     if (idim < size * size) {
-      data_thresh = d_img[idim] - threshold;
+      const T data_thresh = d_img[idim] - threshold;
       idata += (data_thresh > 0) ? data_thresh : 0;
       xdata += (data_thresh > 0) ? data_thresh * x : 0;
       ydata += (data_thresh > 0) ? data_thresh * y : 0;
