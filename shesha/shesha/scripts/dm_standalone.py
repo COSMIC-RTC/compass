ac7c018967041917efb3b2f86668b37421486fff
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2019-02-14 14:35:14 +0100

    simplification of dm_standalone script

--- shesha/shesha/scripts/dm_standalone.py
+++ shesha/shesha/scripts/dm_standalone.py
@@ -14,54 +14,28 @@ plt.ion()
 from shesha.init.geom_init import geom_init_generic
 from shesha.init.dm_init import dm_init_standalone
 
-if (len(sys.argv) == 2):
-    # get parameters from file
-    param_file = sys.argv[1]
-    if (param_file.split('.')[-1] == b"py"):
-        filename = param_file.split('/')[-1]
-        param_path = param_file.split(filename)[0]
-        sys.path.insert(0, param_path)
-        exec("import %s as config" % filename.split(".py")[0])
-        sys.path.remove(param_path)
-    else:
-        raise ValueError("Parameter file extension must be .py")
+#geom
+p_geom = conf.Param_geom()
+geom_init_generic(p_geom, 500)
 
-    # init geom
-    conf.Param_geom.geom_init(config.p_geom, config.p_tel, 750,
-                              config.p_geom.apod)  #apod = apodizer
-else:
-
-    class config:
-        #geom
-        p_geom = conf.Param_geom()
-        geom_init_generic(p_geom, 500)
-
-        #dm
-        p_dm0 = conf.Param_dm()
-        p_dms = [p_dm0]
-        p_dm0.set_type("pzt")
-        # p_dm0.set_pattern("hexa")
-        p_dm0.set_nact(80)
-        p_dm0.set_alt(0.)
-        p_dm0.set_thresh(0.3)
-        p_dm0.set_coupling(0.2)
-        p_dm0.set_unitpervolt(0.01)
-
+#dm
+p_dm0 = conf.Param_dm()
+p_dms = [p_dm0]
+p_dm0.set_type("pzt")
+# p_dm0.set_pattern("hexa")
+p_dm0.set_nact(80)
+p_dm0.set_alt(0.)
+p_dm0.set_thresh(0.3)
+p_dm0.set_coupling(0.2)
+p_dm0.set_unitpervolt(0.01)
+p_dm0.set_pzt_extent(0)
 
 #   context
-c = ch.context.get_instance_ngpu(1, [0])
-# c = ch.carmaWrap_context(devices=np.array([0,1], dtype=np.int32))
-# c.set_activeDevice(0) #useful only if you use ch.carmaWrap_context()
-# c = ch.carmaWrap_context(devices=config.p_loop.devices)
+c = ch.context.get_instance_1gpu(0)
 
-config.p_dm0.set_pzt_extent(0)
 #   dm
 print("->dm")
-if hasattr(config, "p_tel") and config.p_tel is not None:
-    dms = dm_init_standalone(c, config.p_dms, config.p_geom, config.p_tel.diam,
-                             config.p_tel.cobs)
-else:
-    dms = dm_init_standalone(c, config.p_dms, config.p_geom)
+dms = dm_init_standalone(c, p_dms, p_geom)
 
 print("====================")
 print("init done")
