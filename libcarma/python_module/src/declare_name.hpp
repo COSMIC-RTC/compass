db469e4dcb8efb6a6ac21aad4cc0ace0cbd26457
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2019-03-21 17:06:20 +0100

    Hunter integration
    Update hunter integration

--- libcarma/python_module/src/declare_name.hpp
+++ libcarma/python_module/src/declare_name.hpp
@@ -7,37 +7,37 @@
 template <typename T>
 constexpr static char const* explicit_name();
 
-#define DECLARE_NAME(Type, Name)       \
+#define DECLARE_NAME_CARMA(Type, Name)       \
   template <>                          \
   constexpr char const* explicit_name<Type>() { \
     return #Name;                      \
   }
 
-// DECLARE_NAME(u8, Uint8)
-// DECLARE_NAME(i8, Int8)
+// DECLARE_NAME_CARMA(u8, Uint8)
+// DECLARE_NAME_CARMA(i8, Int8)
 
-// DECLARE_NAME(u16, Uint16)
-// DECLARE_NAME(i16, Int16)
+// DECLARE_NAME_CARMA(u16, Uint16)
+// DECLARE_NAME_CARMA(i16, Int16)
 
-// DECLARE_NAME(u32, Uint32)
-// DECLARE_NAME(i32, Int32)
+// DECLARE_NAME_CARMA(u32, Uint32)
+// DECLARE_NAME_CARMA(i32, Int32)
 
-// DECLARE_NAME(u64, Uint64)
-// DECLARE_NAME(i64, Int64)
+// DECLARE_NAME_CARMA(u64, Uint64)
+// DECLARE_NAME_CARMA(i64, Int64)
 
-DECLARE_NAME(int, int);
-DECLARE_NAME(unsigned int, uint);
-DECLARE_NAME(uint16_t, uint16);
+DECLARE_NAME_CARMA(int, int);
+DECLARE_NAME_CARMA(unsigned int, uint);
+DECLARE_NAME_CARMA(uint16_t, uint16);
 
 #ifdef CAN_DO_HALF
-DECLARE_NAME(half, half);
+DECLARE_NAME_CARMA(half, half);
 #endif
-DECLARE_NAME(float, float);
-DECLARE_NAME(double, double);
+DECLARE_NAME_CARMA(float, float);
+DECLARE_NAME_CARMA(double, double);
 
-DECLARE_NAME(cuFloatComplex, float_complex);
-DECLARE_NAME(cuDoubleComplex, double_complex);
-// DECLARE_NAME(tuple_t<float>, tuple_float);
+DECLARE_NAME_CARMA(cuFloatComplex, float_complex);
+DECLARE_NAME_CARMA(cuDoubleComplex, double_complex);
+// DECLARE_NAME_CARMA(tuple_t<float>, tuple_float);
 
 template <typename T>
 std::string appendName(std::string str) {
