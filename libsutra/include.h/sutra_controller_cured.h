a35c9b1b782ab205fd00b3c58030f38304d222fb
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2019-09-10 22:42:07 +0200

    preparation of V4.3.0
    * change license to GNU LGPL-v3
    * add Turing support
    * add AoSupervisor class on top of CompassSupervisor and BenchSupervisor
    * SH WFS can handle big subapertures (before it was limited to 20x20)
    * add LUTpix in calibration process to reorder pixels
    * possibility to compute target SR fitted on a0 sinc function
    * modification of pyramid centroider to use CUB

--- libsutra/include.h/sutra_controller_cured.h
+++ libsutra/include.h/sutra_controller_cured.h
@@ -1,21 +1,46 @@
-/**
- * \file sutra_controller_cured.h
- *
- * \class sutra_controller_cured
- *
- * \ingroup libsutra
- *
- * \brief this class provides the controller_cured features to COMPASS
- *
- * \authors Damien Gratadour & Arnaud Sevin & Florian Ferreira
- *
- * \version 4.2.0
- *
- * \date 2011/01/28
- *
- */
-#ifndef _sutra_controller_cured_H_
-#define _sutra_controller_cured_H_
+// -----------------------------------------------------------------------------
+//  This file is part of COMPASS <https://anr-compass.github.io/compass/>
+//
+//  Copyright (C) 2011-2019 COMPASS Team <https://github.com/ANR-COMPASS>
+//  All rights reserved.
+//  Distributed under GNU - LGPL
+//
+//  COMPASS is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser 
+//  General Public License as published by the Free Software Foundation, either version 3 of the License, 
+//  or any later version.
+//
+//  COMPASS: End-to-end AO simulation tool using GPU acceleration 
+//  The COMPASS platform was designed to meet the need of high-performance for the simulation of AO systems. 
+//  
+//  The final product includes a software package for simulating all the critical subcomponents of AO, 
+//  particularly in the context of the ELT and a real-time core based on several control approaches, 
+//  with performances consistent with its integration into an instrument. Taking advantage of the specific 
+//  hardware architecture of the GPU, the COMPASS tool allows to achieve adequate execution speeds to
+//  conduct large simulation campaigns called to the ELT. 
+//  
+//  The COMPASS platform can be used to carry a wide variety of simulations to both testspecific components 
+//  of AO of the E-ELT (such as wavefront analysis device with a pyramid or elongated Laser star), and 
+//  various systems configurations such as multi-conjugate AO.
+//
+//  COMPASS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+//  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+//  See the GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License along with COMPASS. 
+//  If not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>.
+// -----------------------------------------------------------------------------
+
+//! \file      sutra_controller_cured.h
+//! \ingroup   libsutra
+//! \class     sutra_controller_cured
+//! \brief     this class provides the controller_cured features to COMPASS
+//! \author    COMPASS Team <https://github.com/ANR-COMPASS>
+//! \version   4.3.0
+//! \date      2011/01/28
+//! \copyright GNU Lesser General Public License
+
+#ifndef _SUTRA_CONTROLLER_CURED_H_
+#define _SUTRA_CONTROLLER_CURED_H_
 
 #include <sutra_controller.h>
 
@@ -55,4 +80,4 @@ class sutra_controller_cured : public sutra_controller<Tcomp, Tout> {
   int frame_delay();
 };
 
-#endif  // _sutra_controller_cured_H_
+#endif  // _SUTRA_CONTROLLER_CURED_H_
