aecfbde0c3ad7e4780c00464c145cc14805e3408
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2020-05-25 17:12:56 +0200

    test update doc at every commit on develop

--- doc/generate_coverage.sh
+++ doc/generate_coverage.sh
@@ -3,7 +3,7 @@
 coverage run -m pytest $COMPASS_ROOT/libcarma/python_module/test $SHESHA_ROOT/tests/pytest/rtc || echo "It's just a scratch"
 
 # script="$SHESHA_ROOT/shesha/tests/check.py"
-rm -f check.h5
+rm -f $COMPASS_ROOT/check.h5
 script="tests.check"
 conf_path="$SHESHA_ROOT/data/par/par4tests"
 nb_test=$(ls -1 $conf_path/*.py | wc -l)
@@ -17,4 +17,4 @@ do
     # let "current_test++"
     current_test=$(expr $current_test + 1)
 done
-rm -f check.h5
+rm -f $COMPASS_ROOT/check.h5
