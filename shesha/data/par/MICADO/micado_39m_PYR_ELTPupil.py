640b1adbbf4f099f283edb6fe0ff24e0b4b1599c
Galland Nicolas <nicolas.galland@obspm.fr>
2020-06-05 10:54:56 +0200

    Ngalland/customdmdev

--- shesha/data/par/MICADO/micado_39m_PYR_ELTPupil.py
+++ shesha/data/par/MICADO/micado_39m_PYR_ELTPupil.py
@@ -131,16 +131,6 @@ p_dm0.set_push4imat(0.01)
 p_dm0.set_type_pattern("hexaM4")
 #p_dm0.set_influType("gaussian")
 p_dm0.set_influType("radialSchwartz")
-"""
-p_dm0.set_file_influ_hdf5("/home/fvidal/compass/shesha/data/M4data/elt_influ_spider.h5")
-p_dm0.set_center_name("center")
-p_dm0.set_cube_name("m_influ")
-p_dm0.set_x_name("xpos")
-p_dm0.set_y_name("ypos")
-p_dm0.set_influ_res("res")
-p_dm0.set_diam_dm("diam")
-p_dm0.set_diam_dm_proj("diam_projet")
-"""
 
 #p_dm0.set_gain(0.2)
 
