770091c155256cb164a0d2d2ef0a5004ce36eedd
Sevin Arnaud <arnaud.sevin@obspm.fr>
2020-07-05 10:31:09 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,5 +4,7 @@ LOCAL_DIR="$(realpath --relative-to=$(pwd) $( cd "$( dirname "${BASH_SOURCE[0]}"
 # install dependencies
 pip install -r $LOCAL_DIR/requirements.txt || exit 0
 
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
 # Adds cosmic conan repository if it is not already the case.
 conan remote list | grep cosmic || conan remote add cosmic https://api.bintray.com/conan/odp/cosmic
