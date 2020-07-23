8afafe6ec1cc86f7c89d6c85f8ce96bf7931af8b
Doucet Nicolas <nicolas.doucet@obspm.fr>
2020-07-23 07:24:57 +0200

    change the organisation of fits files

--- shesha/shesha/util/writers/yao/data.py
+++ shesha/shesha/util/writers/yao/data.py
@@ -28,6 +28,26 @@ def get_yao_subap_pos_single(sup, wfs_id):
     valid_Y = (valid_Y-total/2)*toMeter
     return valid_X, valid_Y
 
+def wfs_to_fits_hdu(sup, wfs_id):
+    """Return a fits Header Data Unit (HDU) representation of a single WFS
+
+    Args:
+        sup : (compasSSupervisor) : supervisor
+
+    Kwargs:
+        wfs_id : (int) : index of the WFS in the supervisor
+
+    Return:
+        hdu : (ImageHDU) : fits representation of the WFS
+    """
+    hdu_name = "WFS" + str(wfs_id)
+    X,Y = get_yao_subap_pos_single(sup, wfs_id)
+    valid_subap = np.array([X,Y],dtype=np.float64)
+    hdu = fits.ImageHDU( valid_subap, name=hdu_name)
+    hdu.header["NSSP"] = sup.config.p_wfss[wfs_id].get_nxsub()
+    hdu.header["SSPSIZE"] = sup.config.p_wfss[wfs_id].get_subapd()
+    return hdu
+
 
 def get_yao_subap_pos(sup, *, n_wfs=-1):
 
@@ -79,6 +99,44 @@ def get_yao_actu_pos_single(sup, dm_id):
     dm=sup.config.p_dms[dm_id]
     return dm._xpos+1, dm._ypos+1
 
+def dm_to_fits_hdu(sup, dm_id):
+    """Return a fits Header Data Unit (HDU) representation of a single DM
+
+    Args:
+        sup : (compasSSupervisor) : supervisor
+
+    Kwargs:
+        wfs_id : (int) : index of the DM in the supervisor
+
+    Return:
+        hdu : (ImageHDU) : fits representation of the DM
+    """
+    hdu_name = "DM" + str(dm_id)
+    X,Y = get_yao_actu_pos_single(sup, dm_id)
+    valid_subap = np.array([X,Y],dtype=np.float64)
+    hdu = fits.ImageHDU( valid_subap, name=hdu_name)
+    hdu.header["NACTU"] = sup.config.p_dms[dm_id].get_nact()
+    hdu.header["PITCH"] = sup.config.p_dms[dm_id].get_pitch()
+    hdu.header["COUPLING"] = sup.config.p_dms[dm_id].get_coupling()
+    hdu.header["ALT"] = sup.config.p_dms[dm_id].get_alt()
+    return hdu
+
+def dm_influ_to_fits_hdu(sup, dm_id):
+    """Return a fits Header Data Unit (HDU) holding the influence functions of a specific DM
+
+    Args:
+        sup : (compasSSupervisor) : supervisor
+
+    Kwargs:
+        wfs_id : (int) : index of the DM in the supervisor
+
+    Return:
+        hdu : (ImageHDU) : hdu holding the DM influence functions
+    """
+    hdu_name = "INFLU_DM" + str(dm_id)
+    hdu = fits.ImageHDU( sup.config.p_dms[dm_id].get_influ(), name=hdu_name)
+    return hdu
+
 
 def get_yao_actu_pos(sup):
     """return the coordinates of all  DM actuators for YAO
@@ -103,7 +161,7 @@ def get_yao_actu_pos(sup):
     return n_actu.astype(np.int32), actu_pos.astype(np.float64)
 
 
-def write_data(file_name, sup, *, n_wfs=-1 ,controller_id=0 ,
+def write_data_old(file_name, sup, *, n_wfs=-1 ,controller_id=0 ,
                compose_type="controller"):
     """ Write data for yao compatibility
 
@@ -164,6 +222,85 @@ def write_data(file_name, sup, *, n_wfs=-1 ,controller_id=0 ,
 
     hdul=fits.HDUList([hdu, hdu_nsubap, hdu_subap_pos, hdu_nactu, hdu_actu_pos,
                        hdu_imat, hdu_cmat])
+    hdul.writeto(file_name, overwrite=1)
+
+def write_data(file_name, sup, *, wfss_indices=None, dms_indices=None,
+               controller_id=0, compose_type="controller"):
+    """ Write data for yao compatibility
+
+    write into a single fits:
+        * number of valide subapertures
+        * number of actuators
+        * subapertures position (2-dim array x,y) in meters centered
+        * actuator position (2-dim array x,y) in pixels starting from 0
+        * interaction matrix (2*nSubap , nactu)
+        * command matrix (nacy , 2*nSubap)
+
+    Args:
+        file_name : (str) : data file name
+
+        sup : (compasSSupervisor) : supervisor
+
+        wfss_indices : (list[int]) : optional, default all, list of the wfs indices to include
+
+        dms_indices : (list[int]) : optional, default all, list of the DM indices to include
+
+        controller_id : (int) : index of the controller passed to yao
+
+        compose_type : (str) : possibility to specify split tomography case ("controller" or "splitTomo")
+    """
+    print("writing data to" + file_name)
+    hdul=fits.HDUList([])
+
+    # setting list of wfs and dm
+    conf = sup.config
+    if(wfss_indices is None):
+        wfss_indices = np.arange(len(conf.p_wfss))
+    if(dms_indices is None):
+        dms_indices = np.arange(len(conf.p_dms))
+    
+    #cout the number of lgs
+    n_lgs = 0
+    for i in wfss_indices :
+        if(conf.p_wfss[i].get_gsalt() > 0):
+            n_lgs += 1
+
+    #primary hdu contains only keywords for sanity check
+    hdu = fits.PrimaryHDU(np.zeros(1,dtype=np.int32))
+    hdu.header["DIAM"] = conf.p_tel.get_diam()
+    hdu.header["COBS"] = conf.p_tel.get_cobs()
+    hdu.header["NLGS"] = n_lgs
+    hdu.header["NNGS"] = len(wfss_indices) - n_lgs
+    hdu.header["NDM" ] = len(dms_indices)
+
+    #add primary hdu to list
+    hdul.append(hdu)
+
+    # add wfss
+    for i in wfss_indices:
+        hdul.append( wfs_to_fits_hdu(sup, i))
+
+    # add dm
+    for i in dms_indices:
+        hdul.append(dm_to_fits_hdu(sup, i))
+        hdul.append(dm_influ_to_fits_hdu(sup, i))
+
+    # IMAT
+    imat=compose_imat(sup, compose_type=compose_type,
+                      controller_id=controller_id)
+    hdu_imat=fits.ImageHDU(imat,name="IMAT")
+
+    # CMAT
+    hdu_cmat=fits.ImageHDU(sup.rtc.get_command_matrix(controller_id),
+                           name="CMAT")
+
+    print("\t* number of subaperture per WFS")
+    print("\t* subapertures position")
+    print("\t* number of actuator per DM")
+    print("\t* actuators position")
+    print("\t* Imat")
+    print("\t* Cmat")
+
     hdul.writeto(file_name, overwrite=1)
 
 
