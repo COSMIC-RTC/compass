f154beaa0162d038b1cf7f2f1b8319c591dc79c3
Sevin Arnaud <arnaud.sevin@obspm.fr>
2021-04-16 14:27:34 +0000

    Remove npupils in maskedPix algo

--- libsutra/src.cpp/sutra_centroider_maskedPix.cpp
+++ libsutra/src.cpp/sutra_centroider_maskedPix.cpp
@@ -45,27 +45,30 @@
 //! \copyright GNU Lesser General Public License
 
 #include <sutra_centroider_maskedPix.h>
+
 #include <string>
 
 template <class Tin, class T>
 SutraCentroiderMaskedPix<Tin, T>::SutraCentroiderMaskedPix(
-    CarmaContext *context, SutraWfs *wfs, long nvalid, long npupils,
-    float offset, float scale, bool filter_TT, int device)
+    CarmaContext *context, SutraWfs *wfs, long nvalid, float offset,
+    float scale, bool filter_TT, int device)
     : SutraCentroider<Tin, T>(context, wfs, nvalid, offset, scale, filter_TT,
-                               device) {
+                              device) {
   context->set_active_device(device, 1);
 
   if (wfs != nullptr)
     this->nslopes = wfs->d_validsubsx->get_nb_elements();
   else
-    this->nslopes = nvalid * npupils;
+    this->nslopes = nvalid;
   long dims_data[2] = {1, this->nslopes};
   if (this->d_intensities != nullptr) delete this->d_intensities;
   this->d_intensities = new CarmaObj<float>(context, dims_data);
   this->d_intensities->init_reduceCub();
   long dims_data2[2] = {1, this->nslopes};
   this->d_centroids_ref = new CarmaObj<T>(this->current_context, dims_data2);
+#ifndef CAN_DO_HALF
   this->d_centroids_ref->memset(1);
+#endif
   this->d_selected_pix = nullptr;
   this->d_mask = nullptr;
 
@@ -99,9 +102,9 @@ int SutraCentroiderMaskedPix<Tin, T>::fill_selected_pix(T *pix) {
     this->d_selected_pix = new CarmaObj<T>(this->current_context, dims_data2);
   }
   this->d_selected_pix->reset();
-  pyr_fill_selected_pix(this->d_selected_pix->get_data(), dims_data2[1],
-                        pix, this->d_validx->get_data(),
-                        this->d_validy->get_data(), this->d_validx->get_nb_elements(),
+  pyr_fill_selected_pix(this->d_selected_pix->get_data(), dims_data2[1], pix,
+                        this->d_validx->get_data(), this->d_validy->get_data(),
+                        this->d_validx->get_nb_elements(),
                         this->current_context->get_device(this->device));
   return EXIT_SUCCESS;
 }
@@ -124,26 +127,29 @@ int SutraCentroiderMaskedPix<Tin, T>::fill_mask() {
   }
   this->d_mask->reset();
   pyr_fill_mask(this->d_mask->get_data(), dims_data2[1],
-                        this->d_validx->get_data(),
-                        this->d_validy->get_data(), this->d_validx->get_nb_elements(),
-                        this->current_context->get_device(this->device));
+                this->d_validx->get_data(), this->d_validy->get_data(),
+                this->d_validx->get_nb_elements(),
+                this->current_context->get_device(this->device));
   return EXIT_SUCCESS;
 }
 
 template <class Tin, class T>
 int SutraCentroiderMaskedPix<Tin, T>::get_cog(float *img, float *intensities,
-                                                T *centroids, int nvalid,
-                                                int npix, int ntot, cudaStream_t stream) {
+                                              T *centroids, int nvalid,
+                                              int npix, int ntot,
+                                              cudaStream_t stream) {
   // TODO(Implement SutraCentroiderMaskedPix<Tin, T>::get_cog)
 
   return get_maskedPix(img, intensities, centroids, this->d_validx->get_data(),
-                       this->d_validy->get_data(), this->nvalid, ntot, stream);
+                       this->d_validy->get_data(), ntot, stream);
 }
 
 template <class Tin, class T>
-int SutraCentroiderMaskedPix<Tin, T>::get_maskedPix(
-    float *img, float *intensities, T *centroids, int *subindx, int *subindy,
-    int nvalid, int ns, cudaStream_t stream) {
+int SutraCentroiderMaskedPix<Tin, T>::get_maskedPix(float *img,
+                                                    float *intensities,
+                                                    T *centroids, int *subindx,
+                                                    int *subindy, int ns,
+                                                    cudaStream_t stream) {
   this->current_context->set_active_device(this->device, 1);
 
   fill_intensities(this->d_intensities->get_data(), img, subindx, subindy, ns,
@@ -154,8 +160,9 @@ int SutraCentroiderMaskedPix<Tin, T>::get_maskedPix(
   this->d_intensities->reduceCub(stream);
 
   get_masked_pix<T>(centroids, this->d_centroids_ref->get_data(), img, subindx,
-                  subindy, this->d_intensities->get_o_data(), ns, this->nslopes,
-                  this->current_context->get_device(this->device), stream);
+                    subindy, this->d_intensities->get_o_data(), ns,
+                    this->nslopes,
+                    this->current_context->get_device(this->device), stream);
 
   if (this->filter_TT) {
     this->apply_TT_filter(centroids);
@@ -166,19 +173,19 @@ int SutraCentroiderMaskedPix<Tin, T>::get_maskedPix(
 
 template <class Tin, class T>
 int SutraCentroiderMaskedPix<Tin, T>::get_cog(float *intensities, T *slopes,
-                                                bool noise) {
+                                              bool noise) {
   if (this->wfs != nullptr) {
     if (this->wfs->type == "pyrhr") {
       if (noise || this->wfs->roket == false) {
-        return this->get_maskedPix(
-            *(this->wfs->d_binimg), intensities, slopes,
-            *(this->wfs->d_validsubsx), *(this->wfs->d_validsubsy),
-            this->wfs->nvalid, this->wfs->nfft / this->wfs->nrebin);
+        return this->get_maskedPix(*(this->wfs->d_binimg), intensities, slopes,
+                                   *(this->wfs->d_validsubsx),
+                                   *(this->wfs->d_validsubsy),
+                                   this->wfs->nfft / this->wfs->nrebin);
       } else
-        return this->get_maskedPix(
-            *(this->wfs->d_binimg_notnoisy), intensities, slopes,
-            *(this->wfs->d_validsubsx), *(this->wfs->d_validsubsy),
-            this->wfs->nvalid, this->wfs->nfft / this->wfs->nrebin);
+        return this->get_maskedPix(*(this->wfs->d_binimg_notnoisy), intensities,
+                                   slopes, *(this->wfs->d_validsubsx),
+                                   *(this->wfs->d_validsubsy),
+                                   this->wfs->nfft / this->wfs->nrebin);
     } else
       DEBUG_TRACE("WFS must be pyrhr");
   }
@@ -201,15 +208,15 @@ template class SutraCentroiderMaskedPix<uint16_t, float>;
 #ifdef CAN_DO_HALF
 template <>
 int SutraCentroiderMaskedPix<float, half>::get_cog(float *intensities,
-                                                     half *slopes, bool noise) {
+                                                   half *slopes, bool noise) {
   DEBUG_TRACE("Not implemented for half precision");
   return EXIT_FAILURE;
 }
 
 template <>
 int SutraCentroiderMaskedPix<uint16_t, half>::get_cog(float *intensities,
-                                                        half *slopes,
-                                                        bool noise) {
+                                                      half *slopes,
+                                                      bool noise) {
   DEBUG_TRACE("Not implemented for half precision");
   return EXIT_FAILURE;
 }
