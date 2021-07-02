55e5655bf95e0063f728d1f38b0e5938eed8c30b
raplonu <raplonu.jb@gmail.com>
2021-07-02 15:23:27 +1000

    cmake: Refactors cmake files.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,7 @@
 from conans import ConanFile, CMake, tools
 
-import os
 import subprocess as sp
 from packaging import version
-
 
 def cuda_version():
     cmd_output = sp.run(['nvcc', '--version'], stdout=sp.PIPE).stdout.decode('utf-8')
@@ -11,42 +9,51 @@ def cuda_version():
 
 
 class CompassConan(ConanFile):
-    name = "compass"
-    version = "5.1.0"
-    author = "COMPASS Team <https://github.com/ANR-COMPASS>"
-    url = "https://anr-compass.github.io/compass/"
-    description = "End-to-end AO simulation tool using GPU acceleration"
-    topics = ("Adaptive Optics", "Simulation")
+    name = 'compass'
+    version = '5.1.0'
+    author = 'COMPASS Team <https://github.com/ANR-COMPASS>'
+    url = 'https://anr-compass.github.io/compass/'
+    description = 'End-to-end AO simulation tool using GPU acceleration'
+    topics = ('Adaptive Optics', 'Simulation')
     settings = 'os', 'compiler', 'build_type', 'arch'
-    requires = ['wyrm/0.3@cosmic/stable']
-    generators = 'cmake'
-    options = {"python_build": [True, False], "do_half": [True, False]}
-    default_options = {
-            "python_build": True,
-            "do_half": True,
-            'wyrm:cuda': True,
-            'wyrm:half': True
+    requires = [
+        'wyrm/0.4@cosmic/stable',
+        'emu/0.1@cosmic/stable',
+        ]
+    generators = ['cmake', 'cmake_find_package']
+    options = {
+        'shared'        : [True, False],
+        'fPIC'          : [True, False],
+        'python_build'  : [True, False],
+        'half'          : [True, False],
+        'python_version': 'ANY'
     }
+    default_options = {
+        'shared'      : True,
+        'fPIC'        : True,
+        'python_build': True,
+        'half'        : True,
+        'wyrm:cuda'   : True,
+        'wyrm:half'   : True,
+    }
+
     def requirements(self):
         if cuda_version() < version.parse('11.0'):
             self.requires('cub/1.8.0@cosmic/stable')
 
-    def build(self):
+    def _configure(self):
         cmake = CMake(self)
-        if "COMPASS_DO_HALF" in os.environ:
-            cmake.definitions["do_half"] = os.environ['COMPASS_DO_HALF']
-        # cmake.configure(source_folder="compass")
-        cmake.configure()
-        cmake.build()
-        cmake.install()
+        cmake.definitions['do_half']                 = self.options.half
+        cmake.definitions['build_python_module']     = self.options.python_build
+        cmake.definitions['PYBIND11_PYTHON_VERSION'] = self.options.python_version
+        cmake.definitions['CMAKE_CUDA_ARCHITECTURES'] = self.options['emu'].cuda_sm
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure(source_folder='.')
+
+        return cmake
+
+    def build(self):
+        self._configure().build()
 
     def package(self):
-        self.copy("*", ".", "build/package")  #keep_path default is True
-
-    def package_info(self):
-        self.cpp_info.libs = ["carma", "sutra"]
+        self._configure().install()
