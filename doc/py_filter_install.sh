efc9f0ad94b41c5e255e88c4003b6d883b131000
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2021-07-16 01:17:31 -1000

    fix doc generation

--- doc/py_filter_install.sh
+++ doc/py_filter_install.sh
@@ -2,4 +2,4 @@ git clone https://github.com/Feneric/doxypypy
 
 (cd doxypypy; python -m setup install)
 
-cp py_filter $(dirname $(which python))
+cp $COMPASS_ROOT/doc/py_filter $(dirname $(which python))
