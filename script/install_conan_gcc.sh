9c637c17fcc3448e536712975b82838977d581b8
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2022-12-08 10:49:32 +0100

    debug PupilBoxes

--- script/install_conan_gcc.sh
+++ script/install_conan_gcc.sh
@@ -20,7 +20,6 @@ conan profile update settings.compiler.libcxx=libstdc++11 default
 if [ ! -z $1 ]
 then
     conan profile update settings.compiler.version=$1 default
-fi
 
 # for Arch: CUDA_ROOT=/opt/cuda
 # for Ubuntu 18.04: CUDA_ROOT=/usr/local/cuda
@@ -28,4 +27,5 @@ if [ ! -z $CUDA_ROOT ]
 then
     conan profile update env.CC=$CUDA_ROOT/bin/gcc default
     conan profile update env.CXX=$CUDA_ROOT/bin/g++ default
+fi
 fi
--- shesha/shesha/util/dm_util.py
+++ shesha/shesha/util/dm_util.py
@@ -356,14 +356,14 @@ def make_zernike(nzer: int, size: int, diameter: int, xc=-1., yc=-1., ext=0):
 
         if ext:
             for i in range((n - m) // 2 + 1):
-                z[:, :, zn] = z[:, :, zn] + (-1.) ** i * zrmod ** (n - 2. * i) * float(np.math.factorial(n - i)) / \
-                    float(np.math.factorial(i) * np.math.factorial((n + m) / 2 - i) *
-                          np.math.factorial((n - m) / 2 - i))
+                z[:, :, zn] = z[:, :, zn] + (-1.) ** i * zrmod ** (n - 2. * i) * float(np.math.factorial(n - i)) // \
+                    float(np.math.factorial(i) * np.math.factorial((n + m) // 2 - i) *
+                          np.math.factorial((n - m) // 2 - i))
         else:
             for i in range((n - m) // 2 + 1):
-                z[:, :, zn] = z[:, :, zn] + (-1.) ** i * zr ** (n - 2. * i) * float(np.math.factorial(n - i)) / \
-                    float(np.math.factorial(i) * np.math.factorial((n + m) / 2 - i) *
-                          np.math.factorial((n - m) / 2 - i))
+                z[:, :, zn] = z[:, :, zn] + (-1.) ** i * zr ** (n - 2. * i) * float(np.math.factorial(n - i)) // \
+                    float(np.math.factorial(i) * np.math.factorial((n + m) // 2 - i) *
+                          np.math.factorial((n - m) // 2 - i))
 
         if ((zn + 1) % 2 == 1):
             if (m == 0):
