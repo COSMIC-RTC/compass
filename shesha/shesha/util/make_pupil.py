ed6f831a8436cc5068f06420ce1eea64c49a0298
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2023-03-16 13:19:29 +0100

    follows shesha v5.4.1

--- shesha/shesha/util/make_pupil.py
+++ shesha/shesha/util/make_pupil.py
@@ -1,7 +1,7 @@
 ## @package   shesha.util.make_pupil
 ## @brief     Pupil creation functions
 ## @author    COMPASS Team <https://github.com/ANR-COMPASS>
-## @version   5.4.0
+## @version   5.4.1
 ## @date      2022/01/24
 ## @copyright GNU Lesser General Public License
 #
