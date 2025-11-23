8bc0d60faf8ee8aacd1e8df651f0555cd8bac03c
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2025-11-23 23:49:59 +0100

    Refactor CI/CD configuration and cleanup coverage script

--- doc/generate_coverage.sh
+++ doc/generate_coverage.sh
@@ -8,7 +8,7 @@ coverage run -m pytest $COMPASS_ROOT/python_module/carma/test
 coverage run -a -m pytest $SHESHA_ROOT/tests/pytest/rtc $SHESHA_ROOT/tests/pytest/rtc_standalone $SHESHA_ROOT/tests/pytest/supervisor #|| echo "It's just a scratch"
 
 # script="$SHESHA_ROOT/shesha/tests/check.py"
-rm -f $COMPASS_ROOT/check.h5
+rm -f check.h5
 script="tests.check"
 conf_path="$SHESHA_ROOT/data/par/par4tests"
 nb_test=$(ls -1 $conf_path/*.py | wc -l)
@@ -22,4 +22,6 @@ do
     # let "current_test++"
     current_test=$(expr $current_test + 1)
 done
-rm -f $COMPASS_ROOT/check.h5
+CMD="python $script osef --displayResult --repportResult=report_E2E.md"
+$CMD
+rm -f check.h5
